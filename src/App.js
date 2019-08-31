7cdbe24df660441627322e38b8ee93c666adb513
Seb Lawson <seblawson11@gmail.com>
2019-08-31 13:43:36 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './homepage.component';
-
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
-    <div><HomePage/></div>
+    <div>
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </switch>
+    </div>
   );
 }
 
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'Vegetables',
+          imageUrl: 'https://images.unsplash.com/photo-1452948491233-ad8a1ed01085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2106&q=80',
+          id: 1,
+          linkUrl: 'Vegetables'
+        },
+        {
+          title: 'Fruit',
+          imageUrl: 'https://images.unsplash.com/photo-1414396914239-e70522479d13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+          id: 2,
+          linkUrl: ''
+        },
+        {
+          title: 'Dairy',
+          imageUrl:'https://images.unsplash.com/photo-1489726933853-010eb1484d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2990&q=80',
+          id: 3,
+          linkUrl: ''
+        },
+        {
+          title: 'Other',
+          imageUrl:'https://images.unsplash.com/photo-1448062885262-aa6670248b0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2098&q=80',
+          id: 4,
+          linkUrl: ''
+        },
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <div className='directory-menu'>
+      {
+        this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps}/>
+        ))
+      }
+      </div>
+    )
+  }
+}
+
+export default Directory;
