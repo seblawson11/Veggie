2187a3c312b4663d902f59884ac965a1cec38020
Seb Lawson <seblawson11@gmail.com>
2019-09-01 08:43:28 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
